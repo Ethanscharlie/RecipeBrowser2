6170503aa385cc7c461abd92bd4428370f2cea09
Ethanscharlie <ethanscharlie@protonmail.com>
2025-07-29 23:22:14 -0600

    Added pizza recipe

--- generate.py
+++ generate.py
@@ -4,20 +4,27 @@ import os
 
 
 def main():
+    recipe_name_list = get_recipe_name_list()
+
     with open("index.html", "w+") as f:
-        f.write(generate_index())
+        f.write(generate_index(recipe_name_list))
 
     try:
         os.mkdir("r")
     except FileExistsError:
         pass
 
-    with open("r/sushi.html", "w+") as f:
-        f.write(generate_recipe_html())
+    for recipe_name in recipe_name_list:
+        with open(f"r/{recipe_name}.html", "w+") as f:
+            f.write(generate_recipe_html(recipe_name))
 
 
-def generate_recipe_html() -> str:
-    recipe_yml = load_recipe_yml_from_filename("sushi")
+def get_recipe_name_list() -> list[str]:
+    return [name.split(".")[0] for name in os.listdir("recipes")]
+
+
+def generate_recipe_html(name: str) -> str:
+    recipe_yml = load_recipe_yml_from_filename(name)
 
     html = load_template("recipe")
     html = html.replace("{{ RECIPE_TITLE }}", recipe_yml["Title"])
@@ -37,11 +44,16 @@ def generate_li_from_list(items: list[str]) -> str:
     return "\n".join([f"<li>{d}</li>" for d in items])
 
 
-def generate_index() -> str:
+def generate_index(recipe_names: list[str]) -> str:
     html = load_template("index")
     html = html.replace(
         "{{ RECIPE_CARDS }}",
-        generate_recipe_card(load_recipe_yml_from_filename("sushi")),
+        "\n".join(
+            [
+                generate_recipe_card(load_recipe_yml_from_filename(recipe_name))
+                for recipe_name in recipe_names
+            ]
+        ),
     )
     return html
 
